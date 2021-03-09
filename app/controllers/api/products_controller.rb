ac5e514763f55ba32d481512f32ab16cef26f312
Lil <lil@Lils-MacBook-Air.local>
2021-03-09 08:45:10 -0600

    Adds routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def show
+    @product = Product.find(:id)
+    render "show.json.jb"
+  end
 end
